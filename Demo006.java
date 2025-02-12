4a3443b82417f3c10acca1c04a672672d85d2dd9
durgaprasad-05 <sudadurgaprasad05@gmail.com>
2025-02-12 09:52:44 +0530

    Add files via upload

---
+++ Demo006.java
@@ -0,0 +1,11 @@
+
+public class Demo006 {
+    public static void main(String[] args) {
+        String name = "Durga";
+        String password = "Durga@123";
+        while ((name == "Durga") && (password == "Durga@123")) {
+            System.out.println("Hello Durga");
+            break;
+        }
+    }
+}
