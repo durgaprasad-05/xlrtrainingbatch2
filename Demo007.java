923d31cd092e856b15c4a38cecea8be214f9ed95
durgaprasad-05 <sudadurgaprasad05@gmail.com>
2025-02-12 09:54:33 +0530

    Add files via upload

---
+++ Demo007.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class Demo007 {
+    public static void main(String[] args) {
+        String name;
+        int age;
+        long phno;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Your Name:");
+        name = scanner.nextLine();
+        System.out.println("Your age:");
+        age = scanner.nextInt();
+        System.out.println("Your phno:");
+        phno = scanner.nextLong();
+        System.out.println("hello " + name + " ,your age is " + age + " ,your phone no is " + phno);
+    }
+}
